0afdeda0c0ce0456302f12d44330344909185cc4
matrixlord <th.nokas@gmail.com>
2017-11-07 23:24:46 +0200

    First working prototype

--- data/scripts/images.py
+++ data/scripts/images.py
@@ -1,14 +1,17 @@
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageOps
 from os import listdir
-from os.path import isfile, join
+import random
 
 hotdogs_path = "images/hd"
 cars_path = "images/car"
+test_image = "images/test/test.jpg"
 
 def get_file_data(file_path):
-    data = np.asarray(Image.open(file_path).resize((200, 200), Image.ANTIALIAS))
-    data = np.reshape(data, 120000)
+    image = Image.open(file_path).convert('LA').resize((200, 200), Image.ANTIALIAS)
+    image = ImageOps.grayscale(image)
+    data = np.asarray(image)
+    data = np.reshape(data, 40000)
     data = data/255.0
     return data
 
@@ -18,15 +21,20 @@ def get_all_image_data():
     # Get hotdogs.
     for filename in listdir(hotdogs_path):
         try:
-            images.append([1, get_file_data(hotdogs_path + "/" + filename)])
+            images.append([[1, 0], get_file_data(hotdogs_path + "/" + filename)])
         except IOError:
             print("Found one non image file")
     # Get cars.
     for filename in listdir(cars_path):
         try:
-            images.append([2, get_file_data(cars_path + "/" + filename)])
+            images.append([[0, 1], get_file_data(cars_path + "/" + filename)])
         except IOError:
             print("Found one non image file")
+    
+    # Shuffle images.
+    random.shuffle(images)
 
     return images
 
+def get_test_image():
+    return get_file_data(test_image)
--- data/scripts/tf.py
+++ data/scripts/tf.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
-from images import get_all_image_data 
+from images import get_all_image_data, get_test_image
 
+input_n = 40000
 # Hidden layers.
 l1_n = 2000
 l2_n = 500
@@ -9,23 +10,21 @@ l3_n = 300
 # Output layer.
 n_classes = 2
 
-training_steps = 50
-learning_rate = 0.002
-
-print(get_all_image_data())
+x = tf.placeholder('float')
+y = tf.placeholder('float')
 
 def create_model(data):
-    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([120000, l1_n])),
-                      'biases':tf.Variable(tf.random_normal([l1_n]))}
+    hidden_1_layer = {'weights': tf.Variable(tf.random_normal([input_n, l1_n])),
+                      'biases': tf.Variable(tf.random_normal([l1_n]))}
 
-    hidden_2_layer = {'weights':tf.Variable(tf.random_normal([l1_n, l2_n])),
-                      'biases':tf.Variable(tf.random_normal([l2_n]))}
+    hidden_2_layer = {'weights': tf.Variable(tf.random_normal([l1_n, l2_n])),
+                      'biases': tf.Variable(tf.random_normal([l2_n]))}
 
-    hidden_3_layer = {'weights':tf.Variable(tf.random_normal([l2_n, l3_n])),
-                      'biases':tf.Variable(tf.random_normal([l3_n]))}
+    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([l2_n, l3_n])),
+                      'biases': tf.Variable(tf.random_normal([l3_n]))}
 
-    output_layer = {'weights':tf.Variable(tf.random_normal([l3_n, n_classes])),
-                    'biases':tf.Variable(tf.random_normal([n_classes]))}
+    output_layer = {'weights': tf.Variable(tf.random_normal([l3_n, n_classes])),
+                    'biases': tf.Variable(tf.random_normal([n_classes]))}
 
     l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
     l1 = tf.nn.relu(l1)
@@ -40,11 +39,29 @@ def create_model(data):
 
     return output
 
-def train_model(data):
-    prediction = create_model(data)
-    error = tf.subtract(prediction, target)
-    loss = tf.nn.l2_loss(error)
-    tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+def train_model(x):
+    train_data = get_all_image_data()
+    train_images = [item[1] for item in train_data]
+    train_labels = [item[0] for item in train_data]
 
+    prediction = create_model(x)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = prediction, labels = y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    
+    epochs = 20
     with tf.Session() as sess:
-        sess.run(tf.initialize_all_valiables())
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(epochs):
+            _, epoch_loss = sess.run([optimizer, cost], feed_dict = {x: train_images, y: train_labels})
+
+            print('Epoch: ' + str(epoch) + ' Epoch loss: ' +  str(epoch_loss))
+
+
+        # Check accuracy.
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        # Set [1, 0] for hot dog and [0, 1] for car.
+        print('Accuracy:', accuracy.eval({x: [get_test_image()], y: [[1, 0]]}))
+
+train_model(x)
